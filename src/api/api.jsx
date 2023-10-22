a5ab5c1673d759c48b6eb0e956d5eb1ab2277fb7
suria babaei <suriyalifework60@gmail.com>
2023-10-22 07:55:44 +0330

    create api with axios

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,1 +1,15 @@
+import axios from "axios";
 const BASE_URL = 'http://185.8.172.145:88/swagger/index.html';
+
+const FormSubmit = async (path, data) => {
+    const response = await axios.post(`${BASE_URL}/ ${path}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        Headers: { "Content-Type": "application/json" },
+    });
+    const json = await response.json()
+    return json;
+}
+
+
+export { FormSubmit };
